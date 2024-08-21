e7ac18f1708fb2b1ddf3c12f0f7500c444a07268
jeff.Gaviola <jeffreygaviola95@gmail.com>
2024-08-21 10:17:44 +0800

    MovieQuoteInfo.java

--- Chapter 1 /MovieQuoteInfo.java
+++ Chapter 1 /MovieQuoteInfo.java
@@ -1,1 +1,17 @@
-
+public class MovieQuoteInfo
+{
+  public static void main(String[] args)
+  {
+    String quote = "The only way to deal with an unfree world is to become so absolutely free that your very existence is an act of rebellion.";
+    String movie = "the Rebel ";
+    String character = "Alber Camus";
+    int year = 2019;
+    
+    System.out.println("Quote: " + quote);
+    System.out.println("Movie: " + movie);
+    System.out.println("Character: " + character);
+    System.out.println("Year: " + year);
+        
+    
+  }
+}
