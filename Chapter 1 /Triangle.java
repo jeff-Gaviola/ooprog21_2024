443c19b6dfdbc499443fce38edbb03b52b1a942b
jeff.Gaviola <jeffreygaviola95@gmail.com>
2024-08-21 10:25:28 +0800

    Triangle.java

---
+++ Chapter 1 /Triangle.java
@@ -0,0 +1,19 @@
+public class Triangle
+{
+   public static void main(String[] args)
+   {
+     System.out.println("              I                    ");
+     System.out.println("             III                   ");
+     System.out.println("            IIIII                  ");
+     System.out.println("           IIIIIII                 ");
+     System.out.println("          IIIIIIIII                ");
+     System.out.println("         IIIIIIIIIII               ");
+     System.out.println("        IIIIIIIIIIIII              ");
+
+     
+
+     
+     
+   }
+
+}
