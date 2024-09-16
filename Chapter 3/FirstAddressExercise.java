bbc2b4b54820d0ce40f570253385bcca7b638495
jeff.Gaviola <jeffreygaviola95@gmail.com>
2024-09-16 22:03:23 +0800

    Create FirstAddressExercise.java

---
+++ Chapter 3/FirstAddressExercise.java
@@ -0,0 +1,14 @@
+public class FirstAddressExercise {
+
+    public static void main(String[] args) {
+        System.out.println("First Java application");
+        displayAddress();
+    }
+
+    public static void displayAddress() {
+        System.out.println("Company: Motorola, Inc.");
+        System.out.println("500 W Monroe St");
+        System.out.println("Chicago, IL 60661");
+        System.out.println("USA");
+    }
+}
